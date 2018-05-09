08b1454cc8ad37811a29d4cf6063eb80ebc44016
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2018-05-08 22:21:48 -0400

    Camera

---
+++ Potat/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Potat.Extensions {
+    public static class SpriteBatchExtensions {
+        public static void DrawDebug(this SpriteBatch spriteBatch, SpriteFont font, string[] strings) {
+            Texture2D rect = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+            rect.SetData(new[] { Color.White });
+
+            Dictionary<string, Vector2> messages = new Dictionary<string, Vector2>();
+
+            // Gather measurements
+            for (int i = 0; i < strings.Length; i++) {
+                messages.Add(strings[i], font.MeasureString(strings[i]));
+            }
+
+            // Draw backdrop
+            int length = (int)messages.Max(x => x.Value.X) + 2;
+            int height = (int)messages.Select(x => x.Value.Y).Aggregate((a, b) => b + a);
+
+            spriteBatch.Draw(rect, new Rectangle(0, 0, length, height), Color.Black * 0.75f);
+
+            // Draw strings
+            int offset = 0;
+            foreach (KeyValuePair<string, Vector2> message in messages) {
+                spriteBatch.DrawString(font, message.Key, new Vector2(0, offset * ((int)message.Value.Y)), Color.White);
+                offset++;
+            }
+        }
+    }
+}
--- Potat/Map.cs
+++ Potat/Map.cs
@@ -1,116 +1,26 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Potat.Extensions;
 
 namespace Potat {
-    //public class Scene {
-    //    List<Tile> tiles;
-
-    //    Rectangle bounds;
-    //    double speed;
-    //    Vector2 offset;
-
-    //    Player player;
-
-    //    List<string> state;
-
-    //    SpriteFont OpenSans;
-
-    //    public Scene(Game game) {
-    //        tiles = new List<Tile>();
-
-    //        bounds = new Rectangle(0, 0, 1000, 1000);
-    //        speed = 100;
-
-    //        player = new Player(Vector2.Zero, null, this);
-
-    //        state = new List<string>();
-
-    //        offset = Vector2.Zero;
-
-    //        OpenSans = game.Content.Load<SpriteFont>("fonts/OpenSans");
-    //    }
-
-    //    public void Tick(Game game, GameTime gameTime) {
-    //        state = new List<string>();
-
-    //        var kstate = Keyboard.GetState();
-
-    //        if (kstate.IsKeyDown(Keys.W))
-    //            offset.Y -= (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
-
-    //        if (kstate.IsKeyDown(Keys.S))
-    //            offset.Y += (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
-
-    //        if (kstate.IsKeyDown(Keys.A))
-    //            offset.X -= (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
-
-    //        if (kstate.IsKeyDown(Keys.D))
-    //            offset.X += (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
-
-    //        TODO: STOP IF AT EDGE
-    //        if (bounds.X > 0) {
-    //            state.Add("Far Right");
-    //            bounds.X = 0;
-    //        }
-    //        if (bounds.Y > 0) {
-    //            state.Add("Low");
-    //            bounds.Y = 0;
-    //        }
-
-    //        if (bounds.X + bounds.Width < game.GraphicsDevice.Viewport.Width) {
-    //            state.Add("Far Left");
-    //            bounds.X = game.graphics.PreferredBackBufferWidth - bounds.Width;
-    //        }
-    //        if (bounds.Y + bounds.Height < game.GraphicsDevice.Viewport.Height) {
-    //            state.Add("High");
-    //            bounds.Y = game.graphics.PreferredBackBufferHeight - bounds.Height;
-    //        }
-
-    //        TODO: MOVE CHARICTER IF AT EDGE
-
-    //        TODO: CENTER IF TOO SMALL
-    //         If too thin to cover full screen
-    //        if (bounds.Width < game.GraphicsDevice.Viewport.Width) {
-    //            bounds.X = (game.GraphicsDevice.Viewport.Width / 2) - (bounds.Width / 2);
-    //        }
-    //        If too short to cover full screen
-    //        if (bounds.Height < game.GraphicsDevice.Viewport.Height) {
-    //            bounds.Y = (game.GraphicsDevice.Viewport.Height / 2) - (bounds.Height / 2);
-    //        }
-    //        TODO: PLAYER
-    //    }
-
-    //    public void Render(SpriteBatch spriteBatch, Game game, GameTime gameTime) {
-    //        Texture2D rect = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
-    //        rect.SetData(new[] { Color.White });
-
-    //        spriteBatch.Draw(rect, bounds, Color.White);
-
-
-    //        Player
-
-
-    //        DEGUG
-    //        #region DEBUG
-    //        spriteBatch.DrawDebug($"Top Left: ({bounds.X}, {bounds.Y}) Bottom Right: ({bounds.Width + bounds.X}, {bounds.Height + bounds.Y}) Screen Size: ({game.GraphicsDevice.Viewport.Width}, {game.GraphicsDevice.Viewport.Height})", OpenSans);
-    //        spriteBatch.DrawDebug($"Offset: ({offset.X}, {offset.Y})", OpenSans);
-    //        spriteBatch.DrawDebug($"Too: {String.Join(", ", state)}", OpenSans);
-    //        spriteBatch.EndDebug();
-    //        #endregion
-    //    }
-
-    //}
     public class Scene {
         SpriteFont OpenSans;
 
+        // OBJECTS
         Rectangle camera;
         Rectangle player;
         float playerSpeed = 100;
         Rectangle map;
+
+        // RENDER POSITIONS
+        Vector2 MapPosition => new Vector2(map.X - camera.X, map.Y - camera.Y);
+        Vector2 PlayerPosition => new Vector2(player.X - camera.X, player.Y - camera.Y);
+
 
         public Scene(Game game) {
             OpenSans = game.Content.Load<SpriteFont>("fonts/OpenSans");
@@ -119,19 +29,19 @@ namespace Potat {
 
             player = new Rectangle(0, 0, 100, 100);
 
-            map = new Rectangle(0, 0, 1000, 100);
+            map = new Rectangle(0, 0, 10000, 5000);
         }
 
         public void Tick(Game game, GameTime gameTime) {
             var kstate = Keyboard.GetState();
 
             // Keep camera size == to viewport size
-            camera.Width = 500;//game.GraphicsDevice.Viewport.Width;
-            camera.Height = 500;//game.GraphicsDevice.Viewport.Height;
+            camera.Width = game.GraphicsDevice.Viewport.Width;
+            camera.Height = game.GraphicsDevice.Viewport.Height;
 
             // MOVE PLAYER IF KEY DOWN
             if (kstate.IsKeyDown(Keys.LeftShift))
-                playerSpeed = 200;
+                playerSpeed = 500;
             else
                 playerSpeed = 100;
 
@@ -148,14 +58,14 @@ namespace Potat {
                 player.X += (int)(playerSpeed * gameTime.ElapsedGameTime.TotalSeconds);
 
             // CENTER CAMERA ON PLAYER
-            camera.X = (player.X + (player.Width/2)) - (camera.Width / 2);
+            camera.X = (player.X + (player.Width / 2)) - (camera.Width / 2);
 
-            camera.Y = (player.Y + (player.Height/2)) - (camera.Height / 2);
+            camera.Y = (player.Y + (player.Height / 2)) - (camera.Height / 2);
 
             // KEEP CAMERA ON MAP, NOT BLANK SPACE
             if (camera.Width > map.Width) {
                 camera.X = (map.Width / 2) - (camera.Width / 2);
-            }  else {
+            } else {
                 camera.X = Math.Max(camera.X, 0);
                 camera.X = Math.Min(camera.X, map.Width - camera.Width);
             }
@@ -167,54 +77,61 @@ namespace Potat {
                 camera.Y = Math.Min(camera.Y, map.Height - camera.Height);
             }
 
-            
+
         }
 
         public void Render(SpriteBatch spriteBatch, Game game, GameTime gameTime) {
             Texture2D rect = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
             rect.SetData(new[] { Color.White });
 
-            spriteBatch.Draw(rect, map, Color.White);
-            spriteBatch.Draw(rect, camera, Color.Red);
-            spriteBatch.Draw(rect, player, Color.Green);
+            // Draw all from map pov
+            //spriteBatch.Draw(rect, map, Color.White);
+            //spriteBatch.Draw(rect, camera, Color.Red);
+            //spriteBatch.Draw(rect, player, Color.Green);
+
+            // Draw map base
+            spriteBatch.Draw(rect, new Rectangle((int)MapPosition.X, (int)MapPosition.Y, map.Width, map.Height), Color.White);
+
+            // Draw map tiles
+            bool black = true;
+            bool preblack;
+            for (int x = 0; x < map.Width; x += 200) {
+                preblack = black;
+                for (int y = 0; y < map.Height; y += 200) {
+                    spriteBatch.Draw(rect, new Rectangle((int)MapPosition.X + x, (int)MapPosition.Y + y, 200, 200), black ? Color.Black : Color.White);
+                    black = !black;
+                }
+                if (preblack == black)
+                    black = !black;
+            }
+
+            // Draw player
+            spriteBatch.Draw(rect, new Rectangle((int)PlayerPosition.X, (int)PlayerPosition.Y, player.Width, player.Height), Color.Green);
 
             spriteBatch.DrawDebug(OpenSans, new[] {
                 $"The player is at ({player.X}, {player.Y}) on the map with a speed of {playerSpeed}",
-                $"The map is {map.Width} by {map.Height}",
-                $"The camera can see from ({camera.X}, {camera.Y}) to ({camera.X + camera.Width}, {camera.Y + camera.Height}), a {camera.Width}x{camera.Height} area"
+                $"The map is {map.Width} by {map.Height} and offset <{MapPosition.X}, {MapPosition.Y}>",
+                $"The camera can see from ({camera.X}, {camera.Y}) to ({camera.X + camera.Width}, {camera.Y + camera.Height}), a {camera.Width}x{camera.Height} area",
+                // TODO
+                //$"The map has {null} tiles, {null} visible and {null} offscreen: {null} of which solid tiles, {null} transparent, and {null} doorways",
+                //$"There are {null} entities in the map, {null} of which are moving, and {null} of witch with active pathfinding",
+                //$"There are {null} collisions happening in the map"
             });
         }
     }
 
-    public static class Extensions {
-        public static void DrawDebug(this SpriteBatch spriteBatch, SpriteFont font, string[] strings) {
-            Texture2D rect = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
-            rect.SetData(new[] { Color.White });
+    //public abstract class Tile {
+    //    public Vector2 position;
+    //    public Texture2D texture;
+    //    public Rectangle bounds;
+    //    public bool solid;
+    //    public float layer;
 
-            for (int i = 0; i < strings.Length; i++) {
+    //    public Tile(Vector2 position, Texture2D texture, bool solid = true, float layer = 0) {
+    //        this.position = position;
+    //        this.texture = texture;
+    //        this.bounds = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
 
-                String debug = strings[i];
-
-                Vector2 measure = font.MeasureString(debug);
-
-                spriteBatch.Draw(rect, new Rectangle(0, i * ((int)measure.Y), (int)measure.X, (int)measure.Y), Color.Black * 0.75f);
-                spriteBatch.DrawString(font, debug, new Vector2(0, i * ((int)measure.Y)), Color.White);
-            }
-        }
-    }
-
-    public abstract class Tile {
-        public Vector2 position;
-        public Texture2D texture;
-        public Rectangle bounds;
-        public bool solid;
-        public float layer;
-
-        public Tile(Vector2 position, Texture2D texture, bool solid = true, float layer = 0) {
-            this.position = position;
-            this.texture = texture;
-            this.bounds = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
-
-        }
-    }
+    //    }
+    //}
 }
